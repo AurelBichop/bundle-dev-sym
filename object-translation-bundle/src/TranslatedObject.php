4a0d40dfa58f22f34e748300e957eb7512727477
AurelBichop <linux.aurelien@gmail.com>
2025-10-09 22:08:52 +0200

    TDD translations

--- object-translation-bundle/src/TranslatedObject.php
+++ object-translation-bundle/src/TranslatedObject.php
@@ -13,20 +13,32 @@ final class TranslatedObject
 {
     /**
      * @param T $_inner
+     * @param array<string,string> $_translations
      */
     public function __construct(
         private object $_inner,
+        private array $_translations,
     ){
     }
 
     public function __call(string $name, array $arguments): mixed
     {
-        return $this->_inner->$name(...$arguments);
+        if ($translatedValue = $this->translatedValue($name)){
+            return $translatedValue;
+        }
+
+        $method = $name;
+
+        if (!method_exists($this->_inner, $name)) {
+            $method = 'get'.ucfirst($name);
+        }
+
+        return $this->_inner->$method(...$arguments);
     }
 
     public function __get(string $name):mixed
     {
-        return $this->_inner->$name;
+        return $this->_translations[$name] ?? $this->_inner->$name;
     }
 
     public function __isset(string $name): bool
@@ -34,5 +46,19 @@ final class TranslatedObject
         return isset($this->_inner->$name);
     }
 
+    private function translatedValue(string $name): ?string
+    {
+        if(isset($this->_translations[$name])) {
+            return $this->_translations[$name];
+        }
+
+        if (!str_starts_with($name, 'get')) {
+            return null;
+        }
+
+        $property = lcfirst(substr($name, 3));
+
+        return $this->_translations[$property] ?? null;
+    }
 
 }
